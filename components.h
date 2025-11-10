ff41f30e79823e32e06713660c01d8c269875d25
Fernando Martinez <105515737+Blazetortoise@users.noreply.github.com>
2025-11-10 13:34:04 -0700

    Update and rename resistors.cpp to components.h
    added node class and changed this to a .h file

---
+++ components.h
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+class Resistor{
+
+protected:
+      int res;
+      double resVoltage;
+      double resCurrent;
+public:
+      
+      Resistor(): {
+            int input; //Every time a resistor is created you are asked for the value
+            cout<< "What value should this resistor have? " <<endl;
+            cin>input;
+            res = input;
+      }
+  void print(){ //make this an overloaded insertion operator later
+      cout << "Resistor with Resistance: " << res << " Current: " << current << " Voltage: " << voltage<<endl;
+    }
+      double getValue() {return res; }
+};
+
+class Node{ // not really a "node" as they are in nodal analysis, basically just a resistor honestly 
+protected:
+      int position; // since there are going to be multiple nodes in a circuit, might add this to circuit class instead idk
+      Resistor *resistors; // for dynamic memory allocation later
+      int rtot; // total resistance to be counted as resistors are created
+      double nodeVoltage;
+      int numResistors; //Handy for current division later
+public: 
+      Node(){
+            string type; // input variables
+            int num; 
+
+      resistors = new Resistor[num];
+            
+      
+      }
+
+
+};
