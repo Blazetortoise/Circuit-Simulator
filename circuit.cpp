b53eb858d86a96d750e73883ecacc9e9673d8a06
ottol1 <162608916+ottol1@users.noreply.github.com>
2025-11-11 21:16:31 -0700

    Enhance Circuit class with node and voltage features
    Refactor Circuit class to include node management and voltage calculations.

--- circuit.cpp
+++ circuit.cpp
@@ -1,6 +1,8 @@
 //I'm going to make this the main file for now since we have like a billion others
 #include <iostream>
-#include "resistors.h"
+#include <vector>
+#include "components.h"
+#include "source.h"
 using namespace std;
 
 
@@ -11,35 +13,69 @@ protected:
     Ground ground;
 
 public:
-    Circuit(){}
+    Circuit(const Source &src, const Ground &gnd) : 
+    source(src), ground(gnd), totalResistance(0.0), totalCurrent(0.0) {}
 
-    void nodecalc(){
-        if(!voltagesource){
-            cout << "No voltage source" << endl;
+    void buildC(){
+        cout << "how many nodes, not including GND: " ;
+        cin >> numNodes;
+            if (numNodes <= 0){
+                throw invalid_argument("Invalid input");
+            }
+        
+
+    for(int i = 0; i < numNodes; i++){
+        Node n(i);
+        char resp;
+
+        cout << "would you like to add resistors to node " << i << "(y/n)";
+        cin resp;
+
+        while(resp == y){
+            double value;
+            cout << "enter resistor value (ohms): "
+            cin >> value;
+            n.addResistor(value);
+
+            cout << "would you like to add another resistor to node " << i << "(y/n)";
+            cin >> resp;
         }
-
-
-    void addResistor(double r) {
-        resistors.push_back(Resistor(r));
+    nodes.push_back(n);
     }
-        
-        double totalR, Vs, cur;
-        
+}
+
+
+
+    void CalcVoltage() {
+        try{
+            totalres = 0.0;
+
+            for(numNodes){
+                totalres += n.totalres;
+            }
+            if(totalres <= 0){
+                throw runtime_error("invalid circuit, resistance cannot be 0 or negative");
+            }
+
+            double vs = source.getVoltage(); //gets voltage from user input
+            totalC = vs / totalres; // v= i*r -> i = v/r
+
+            cout << "**voltage division**" << endl;
+            cout << "Total resistance: " << totalres << " ohms" << endl;
+            cout << "current: " << totalc << " A" << endl;
+            
+//need math 
+        }
+    }
+    
+    void printCircuit() const{
+        cout << "*****************" << endl;
+        source.print();
+
+        for(const auto &n : nodes)
+            cout << n;
     }
 
 
 };//end circuit class
 
-int main(){
-
-circuit Circuit();
-
-}
-
-
-
-int main()
-{
-    
-    return 0;
-}
