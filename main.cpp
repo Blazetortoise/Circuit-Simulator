266b256d226871f0d6569795d503256c6d01b33b
Fernando Martinez <105515737+Blazetortoise@users.noreply.github.com>
2025-11-17 13:47:32 -0700

    Add some of the basic functions to main

--- main.cpp
+++ main.cpp
@@ -2,3 +2,13 @@
 #include <string>
 #include "Circuit.h"
 using namespace std;
+
+int main(){
+
+  Circuit circuit(5,"Circuit1");
+  circuit.buildCircuit();
+  circuit.analyzeCircuit();
+  circuit.printAnalysis();
+
+  return 0;
+}
