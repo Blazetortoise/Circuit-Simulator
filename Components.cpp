761d163162364a8d4585d874aa4f89db5a07fd99
ottol1 <162608916+ottol1@users.noreply.github.com>
2025-11-17 12:16:14 -0700

    Update Components.cpp

--- Components.cpp
+++ Components.cpp
@@ -1,5 +1,62 @@
+#include "Components.h"
+#include <stdexcept>
+#include <iomanip>
+#include <cmath>
+
+
+Resistor::Resistor(double r) : resistance(r), voltage(0.0), current(0.0) {
+    if (r <= 0) {
+        throw invalid_argument("invalid resistance");
+    }
+}
+
+Node::Node(int num) : nodeNumber(num), nodeVoltage(0.0) {
+    cout << "Node " << nodeNumber << " created" << endl;
+}
+
+Node::~Node() {
+    cout << "Node " << nodeNumber << " destroyed" << endl;
+}
+
+
+double Resistor::getResistance() const { return resistance;} //inline get func
+double Resistor::getVoltage() const { return voltage;}
+double Resistor::getCurrent() const { return current;}
+
+double Node::getVoltage() const { return nodeVoltage; }
+int Node::getNodeNumber() const { return nodeNumber;}
+
+
+void Resistor::setVoltage(double v) {//
+    voltage = v;
+    current = voltage / resistance;
+}
+
+void Resistor::setCurrent(double i) {
+    current = i;
+    voltage = current * resistance;
+}
 
 
 
 
-Resistor(double r : resistance(r), voltage(v), current(c)){}
+
+ostream& operator<< (ostream& os, const Resistor& r) {//overload operators
+    os << "Resistor [R = " << r.resistance << " ohms, V=" 
+       << r.voltage << "V, I=" << r.current << "A]";
+    return os;
+}
+ostream& operator<<(ostream& os, const Node& node) {
+    node.print();
+    return os;
+}
+
+
+
+
+
+
+
+
+
+
